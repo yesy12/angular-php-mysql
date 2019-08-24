50e559eefa1d3ebd9351be7c47e926182a597329
Alison <alisonvieira.av29@gmail.com>
2019-08-24 12:02:46 -0300

    ngModel nos inputs, criado funcao proximo e cadastrar, salvado no mysql, colocado opcao de guiche e senha atual

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient,HttpHeaders } from "@angular/common/http";
+import { } from "@a"
+import { environment } from 'environments/environment.prod';
+
 declare var $:any;
+
 @Component({
     selector: 'cadastro-cmp',
     moduleId: module.id,
@@ -7,11 +12,63 @@ declare var $:any;
 })
 
 export class CadastroComponent implements OnInit{
-        
-    constructor(){
+    public headers = new HttpHeaders();
+    public paciente : any;
+
+    public resumo : any = {
+        meuGuiche       : 0,
+        senhaAtual      : 0,
+        totalCadastrado : 0
+    }
+
+    constructor(public http : HttpClient){
+        this.headers = this.headers.set("Content-Type","application/json; charset=utf8")
         
     }
     
     ngOnInit(){
+        this.paciente = {
+            email : null,
+            nome : null,
+            snome : null,
+            endereco : null,
+            cidade : null,
+            estado : null,
+            cep : null,
+            obs : null
+        }
     }
+
+    cadastrar(){
+        let opt : any = {
+            opt : 2,
+            paciente : this.paciente
+        }
+        
+        this.http.post("http://192.168.0.3/aluno/ws/alison/",
+        JSON.stringify(opt),{headers : this.headers})
+            .subscribe(data => {
+                if(data != null){
+                    alert("criado")
+                }
+            });
+        
+    }
+
+    proximo(){
+        let post : any = {
+            opt : 3,
+            guiche : this.resumo.meuGuiche
+        }
+        this.http.post("http://192.168.0.3/aluno/ws/alison/",
+        JSON.stringify(post),
+            {headers : this.headers})
+            .subscribe(data =>{
+                if(data != null){
+                    console.log(data)
+                    this.resumo.senhaAtual = data[0]["senha"];
+                }
+            });  
+    }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl : 'http://192.168.0.14/'
+  apiUrl : 'http://192.168.0.3/aluno/ws/alison/'
 };
